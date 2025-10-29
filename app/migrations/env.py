7ce2f060f1f5004c6d1e88c631a00c461f38a550
Lucas Morais <lucas.ecomp2012@gmail.com>
2025-10-29 20:02:02 -0300

    Adding alembic migrations

---
+++ app/migrations/env.py
@@ -0,0 +1,54 @@
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+
+from alembic import context
+from app.db.base import Base
+from app.config.settings import DATABASE_URL
+# sys .path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+def run_migrations_offline():
+    """Executa migrations no modo offline (sem conexão real)."""
+    url = DATABASE_URL
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL não foi configurada corretamente.")
+    connectable = create_async_engine(
+        DATABASE_URL,
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
