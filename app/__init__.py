f8f63f3b4f4c194082c09e1a0f4726c156f84449
Lucas Morais <lucas.ecomp2012@gmail.com>
2025-10-29 14:05:38 -0300

    adding init py to app

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@
 Pacote principal da aplicação Streamlit Baygon.
 Contém a configuração inicial e os submódulos principais.
 """
-from . import core, config, components, pages
+from . import core, config, schemas, db, api
 
 __version__ = "0.1.0"
-__all__ = ["core", "config", "components", "pages"]
+__all__ = ["core", "config", "db", "api", "schemas"]
